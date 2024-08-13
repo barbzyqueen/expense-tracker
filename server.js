e95aa8e83dd620d75c88455a0d606caa54594fb1
barbzyqueen <barbaramangiri@gmail.com>
2024-08-13 02:53:34 +0100

    Updated server.js to serve static files

--- server.js
+++ server.js
@@ -279,19 +279,19 @@ app.get('/check-session', (req, res) => {
 });
 
 // Serve the login page
-// app.get('/api/login', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/login.html'));
-// });
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/login.html'));
+});
 
 // // // Serve the registration page
-// app.get('/api/register', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/register.html'));
-// });
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/register.html'));
+});
 
-// // // Serve the expenses page (protected route)
-// app.get('/api/expenses', authenticateUser, (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+// // Serve the expenses page (protected route)
+app.get('/expenses', authenticateUser, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Serve the homepage
 app.get('/', (req, res) => {
